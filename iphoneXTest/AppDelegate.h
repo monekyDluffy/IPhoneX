b3f3930a72faa394802754f414c156002c9fb6cc
yusheng <yusheng.liu@lianbi.com.cn>
2017-12-13 16:41:07 +0800

    Initial Commit

---
+++ iphoneXTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iphoneXTest
+//
+//  Created by yusheng on 13/12/2017.
+//  Copyright © 2017 yusheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iphoneXTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iphoneXTest
+//
+//  Created by yusheng on 13/12/2017.
+//  Copyright © 2017 yusheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
